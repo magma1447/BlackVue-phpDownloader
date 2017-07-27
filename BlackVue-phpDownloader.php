86af47b1ad3020e565cbc02ccfe1eeb59e2c7439
Magnus Månsson <magma@1447.se>
2017-07-27 23:41:18 +0200

    Added an optional report after each fetch Different exit codes for different cases Added lock-file, very needed Rewrote config

--- BlackVue-phpDownloader.php
+++ BlackVue-phpDownloader.php
@@ -1,7 +1,17 @@
 <?php
 
-$hostname = 'blackvue';
-$destination = '/data/blackvue/';
+define('EXIT_SUCCESS', 0);
+define('EXIT_PARTIAL', 1);
+define('EXIT_ALREADYRUNNING', 2);
+define('EXIT_NOCONNECT', 3);
+define('EXIT_FILELISTPARSEERROR', 10);
+
+$config = array(
+	'pidFile' => '/tmp/BlackVue-phpDownloader.pid',
+	'hostname' => 'blackvue',
+	'destination' => '/data/blackvue/',
+	'printSummary' => TRUE,
+	);
 
 
 function echoerr($string) {
@@ -9,15 +19,32 @@ function echoerr($string) {
 }
 
 
+
+if(file_exists($config['pidFile'])) {
+	$oldPid = file_get_contents($config['pidFile']);
+	if(file_exists("/proc/{$oldPid}")) {
+		$cmd = file_get_contents("/proc/{$oldPid}/cmdline");
+		if(strpos($cmd, $argv[0]) !== FALSE) {
+			echo "Already running\n";
+			exit(EXIT_ALREADYRUNNING);
+		}
+	}
+}
+$pid = getmypid();
+file_put_contents($config['pidFile'], $pid);
+
+
+
 // Make sure destination has a trailing slash, it's assumed later on in the code
-$destination = rtrim($destination, '/') . '/';
+$destination = rtrim($config['destination'], '/') . '/';
+$report = array();
 
 
 $ch = curl_init();
 curl_setopt_array(
 	$ch,
 	array(
-		CURLOPT_URL => "http://{$hostname}/blackvue_vod.cgi",
+		CURLOPT_URL => "http://{$config['hostname']}/blackvue_vod.cgi",
 		CURLOPT_CONNECTTIMEOUT => 10,
 		CURLOPT_RETURNTRANSFER => TRUE,
 		CURLOPT_LOW_SPEED_LIMIT => 1024*100,	// 100 kB/s
@@ -28,8 +55,8 @@ curl_setopt_array(
 $rsp = curl_exec($ch);
 $errno = curl_errno($ch);
 if(empty($rsp) || $errno !== 0) {
-	echoerr("Error when fetching list, errno: {$errno}\n");
-	exit(1);
+	echo "Error when fetching list, errno: {$errno}\n";
+	exit(EXIT_NOCONNECT);
 }
 $filenames = explode("\r\n", $rsp);
 
@@ -47,19 +74,21 @@ $filenames = array_map(function($line) {
 
 	var_dump($line);
 	echoerr("Error: Failed to parse line\n");
-	exit(1);
+	exit(EXIT_FILELISTPARSEERROR);
 }, $filenames);
 
 
 $cnt = count($filenames);
 echo "Found {$cnt} filenames, will remove those that already exists\n";
+$report[] = "Found {$cnt} filenames in the camera";
 
 $filenames = array_filter($filenames, function($filename) use ($destination) {
 	return (file_exists($destination . $filename)) ? FALSE : TRUE;
 });
-$cnt = count($filenames);
-$estimatedSeconds = $cnt*25;
-echo "{$cnt} files left to fetch, estimated time to fetch all is {$estimatedSeconds} seconds\n";
+$filesToFetch = count($filenames);
+$estimatedSeconds = $filesToFetch*25;
+echo "{$filesToFetch} files left to fetch, estimated time to fetch all is {$estimatedSeconds} seconds\n";
+$report[] = "{$filesToFetch} filenames left to fetch";
 
 
 // Sort the array to fetch newest first
@@ -67,16 +96,19 @@ rsort($filenames);
 
 
 echo PHP_EOL;
+$numFetched = 0;
+$success = TRUE;
 foreach($filenames as $filename) {
 	echo "Fetching file {$filename}\n";
 
 	$t1 = microtime(TRUE);
-	curl_setopt($ch, CURLOPT_URL, "http://{$hostname}/Record/{$filename}");
+	curl_setopt($ch, CURLOPT_URL, "http://{$config['hostname']}/Record/{$filename}");
 	$rsp = curl_exec($ch);
 	$errno = curl_errno($ch);
 	if(empty($rsp) || $errno !== 0) {
 		echoerr("Error when fetching file {$filename}, errno: {$errno}\n");
-		exit(1);
+		$success = FALSE;
+		break;
 	}
 	$t2 = microtime(TRUE);
 
@@ -88,9 +120,26 @@ foreach($filenames as $filename) {
 	echo "Saving file {$filename}\n";
 	file_put_contents($destination . $filename, $rsp);
 
+	$numFetched++;
 	echo PHP_EOL;
 }
+curl_close($ch);
+
 
 echo "All files fetched\n";
-curl_close($ch);
-exit(0);
+if($success) {
+	$report[] = "All {$numFetched} files fetched";
+} else {
+	$report[] = "Fetched {$numFetched} of {$filesToFetch} files";
+}
+
+if($config['printSummary']) {
+	$report = implode(PHP_EOL, $report) . PHP_EOL;
+	echoerr($report);
+}
+
+if($success) {
+	exit(EXIT_SUCCESS);
+} else {
+	exit(EXIT_PARTIAL);
+}
